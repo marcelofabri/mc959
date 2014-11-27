bce86a4d3df14b02257e248e03b3ce1b47c99be4
Marcelo Fabri <marcelofabrimf@gmail.com>
2014-11-27 11:18:33 -0200

    budget

--- multistart.py
+++ multistart.py
@@ -43,13 +43,59 @@ def read_villains(filename):
     return villains
 
 
+def avg_attributes(characters_ids, all_characters):
+    sum_power_grid = 0
+    sum_popularity = 0
+    vt_cost = 0
+    keys = ['Intelligence','Strength', 'Speed', 'Durability', 'Energy Projection', 'Fighting Skills']
+    for characters_id in characters_ids:
+        character = all_characters[characters_id]
+        local_sum = 0
+        for key in keys:
+            local_sum += character[key]
+        sum_popularity += character['Number of Comic Books Where Character Appeared']
+        sum_power_grid += local_sum
+        vt_cost += character['Number of Comic Books Where Character Appeared'] * float(local_sum) / len(keys)
+
+    qtd = len(characters_ids)
+    avg_power_grid = float(sum_power_grid) / len(keys) / qtd
+    avg_popularity = float(sum_popularity) / qtd
+    return avg_power_grid, avg_popularity, vt_cost
+
+
+def budget(villains_ids, all_characters):
+    heroes_ids = []
+    all_villains_ids = []
+    for character in all_characters.values():
+        if character['Hero or Villain'] == 'hero':
+            heroes_ids.append(character['Character ID'])
+        else:
+            all_villains_ids.append(character['Character ID'])
+
+    avg_villains_attributes = avg_attributes(villains_ids, all_characters)
+    avg_all_villains_attributes = avg_attributes(all_villains_ids, all_characters)
+    avg_heroes_attributes = avg_attributes(heroes_ids, all_characters)
+    ratio_pg = avg_heroes_attributes[0] / avg_villains_attributes[0]
+    ratio_pop = avg_heroes_attributes[1] / avg_villains_attributes[1]
+    vt_cost = avg_villains_attributes[2]
+
+    expr1 = ratio_pg * ratio_pop * vt_cost
+
+    factor = avg_villains_attributes[0] / avg_all_villains_attributes[0]
+    avg_pg = avg_heroes_attributes[0]
+    avg_pop = avg_heroes_attributes[1]
+    vt_size = len(villains_ids)
+
+    expr2 = factor * avg_pg * avg_pop * vt_size
+
+    return max(expr1, expr2)
+
+
 def main(filename):
     villains = read_villains(filename)
-    print villains
     characters = read_characters('characters.csv')
-    print characters
+    print budget(villains, characters)
     collaboration = read_collaboration('characters_collaboration.csv')
-    print collaboration
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
